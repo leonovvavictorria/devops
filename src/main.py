27e1f84e331c7e9cf04693d627aa1b0275a3c24a
leonovvavictorria <78730431+leonovvavictorria@users.noreply.github.com>
2022-10-13 17:56:51 +0300

    add gitignore

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from flask import Flask,abort,request
+
+def divide(a,b):
+    if b == 0:
+        return 'Нельзя делить на ноль'
+    return a/b
+
+main = Flask(__name__)
+
+@main.route('/', methods=["GET"])
+def get_tasks():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    res = None
+
+    if b == 0:
+        res = 'Нельзя делить на ноль'
+    elif a and b:
+        res = divide(a, b)
+
+    if res == None:
+        abort(422,"Unprocessable Entity")
+    return {'res': res}
+
+
+if __name__ == '__main__':
+    main.run(host="127.0.0.1", port=5000)
