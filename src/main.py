4bf5b305b32d322b53085685fedfec8f455339a8
leonovvavictorria <78730431+leonovvavictorria@users.noreply.github.com>
2022-10-13 21:43:11 +0300

    tests hopeless 12

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, abort, request
 
 
-def divide(a: int, b: int) -> any:
+def divide(a: int, b: int) -> float | str | int | object:
     if b == 0:
         return 'Нельзя делить на ноль'
     return float(a/b)
